b2c38c2020dae056e4de661a53d300788470c70b
Josh Stricklin <jostricklin@gmail.com>
2016-03-13 16:59:46 -0500

    get voting test to pass

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import helpers so test can 'see' components on the page
 import {
-	renderIntoDocument
+	renderIntoDocument,
+	scryRenderedDOMComponentsWithTag
 } from 'react-addons-test-utils';
 import Voting from '../../src/components/Voting';
+import {expect} from 'chai';
 
 describe('Voting', () => {
 
@@ -11,6 +14,11 @@ describe('Voting', () => {
 		const component = renderIntoDocument(
 			<Voting pair={["Trainspotting", "28 Days Later"]} />
 		);
+		const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+		expect(buttons.length).to.equal(2);
+		expect(buttons[0].textContent).to.equal('Trainspotting');
+		expect(buttons[1].textContent).to.equal('28 Days Later');
 	});
-	
+
 });
