c6cbdb936176c3145d463bd14a5b3eeede9a1cf0
Josh Stricklin <jostricklin@gmail.com>
2016-03-12 22:31:02 -0600

    test helpers for jsdom and react stuff

---
+++ test/test_helper.js
@@ -0,0 +1,17 @@
+import jsdom from 'jsdom';
+
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+const win = doc.defaultView;
+
+// create jsdom versions of objects that are normally provided by the brower
+// this lets them be discovered by react for testing
+global.document = doc;
+global.window = win;
+
+// hoist jsdom objects on to node.js global object
+// this lets properties provided by window be used without a prefix
+Object.keys(window).forEach((key) => {
+  if (!(key in global)) {
+    global[key] = window[key];
+  }
+});
