d8b85e18f047732182b5cb37ea666b4b952ea63f
Josh Stricklin <jostricklin@gmail.com>
2016-03-13 17:03:14 -0500

    add test for correct vote on button click

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import helpers so test can 'see' components on the page
+// add simulate for 'click' testing
 import {
 	renderIntoDocument,
-	scryRenderedDOMComponentsWithTag
+	scryRenderedDOMComponentsWithTag,
+	Simulate
 } from 'react-addons-test-utils';
 import Voting from '../../src/components/Voting';
 import {expect} from 'chai';
@@ -19,6 +21,21 @@ describe('Voting', () => {
 		expect(buttons.length).to.equal(2);
 		expect(buttons[0].textContent).to.equal('Trainspotting');
 		expect(buttons[1].textContent).to.equal('28 Days Later');
+	});
+
+	it('invokes callback when a button is clicked', () => {
+		let votedWith;
+		const vote = (entry) => votedWith = entry;
+
+		const component = renderIntoDocument(
+			<Voting pair={["Trainspotting", "28 Days Later"]}
+							vote={vote} />
+		);
+		// simulate a click on the first button, check for correct result
+		const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+		Simulate.click(buttons[0]);
+
+		expect(votedWith).to.equal('Trainspotting');
 	});
 
 });
