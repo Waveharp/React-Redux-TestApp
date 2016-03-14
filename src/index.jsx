094ce8f349ac286cc76caf80a3b09e22f018711d
Josh Stricklin <jostricklin@gmail.com>
2016-03-13 19:34:46 -0500

    set up first react Route

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Route} from 'react-router';
+import App from './components/App';
 import Voting from './components/Voting';
 
 const pair = ['Trainspotting', '28 Days Later'];
 
-console.log('It is alive!');
+const routes = <Route component={App}>
+	<Route path="/" component={Voting} />
+</Route>;
 
 ReactDOM.render(
 	<Voting pair={pair} winner="Trainspotting" />,
