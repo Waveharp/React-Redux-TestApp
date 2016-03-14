7909eddc3fcd26bfee92894928ea35bcb0f78090
Josh Stricklin <jostricklin@gmail.com>
2016-03-13 19:45:41 -0500

    configure routes for, render Results component

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
 import App from './components/App';
 import Voting from './components/Voting';
+import Results from './components/Results';
 
 
 // point route to voting component
 // specify App as root route
 const routes = <Route component={App}>
+	<Route path="/results" component={Results} />
 	<Route path="/" component={Voting} />
 </Route>;
 
