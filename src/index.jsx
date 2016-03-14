b4a50777ee2e6a792bec8362cd5dbe2e977c7fb8
Josh Stricklin <jostricklin@gmail.com>
2016-03-13 20:28:53 -0500

    set up Redux Store

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
+import {createStore} from 'redux';
+import reducer from './reducer';
 import App from './components/App';
 import Voting from './components/Voting';
 import Results from './components/Results';
 
+// set up the store
+// (TEMPORARY until we get real data) - dispatch SET_STATE action on it
+const store = createStore(reducer);
+store.dispatch({
+	type: 'SET_STATE',
+	state: {
+		vote: {
+			pair: ['Sunshine', '28 Days Later'],
+			tally: {Sunshine: 2}
+		}
+	}
+});
 
 // point route to voting component
 // specify App as root route
