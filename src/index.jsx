b5487c5bee18dc4b9061f5550901fa1b21a16f33
Josh Stricklin <jostricklin@gmail.com>
2016-03-13 20:32:14 -0500

    use Provider from react-redux to connect component tree to redux store

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
 import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import reducer from './reducer';
 import App from './components/App';
 import Voting from './components/Voting';
@@ -27,7 +28,10 @@ const routes = <Route component={App}>
 	<Route path="/" component={Voting} />
 </Route>;
 
+// connect our component tree to Redux store with Provider
 ReactDOM.render(
-	<Router history={hashHistory}>{routes}</Router>,
+	<Provider store={store}>
+		<Router history={hashHistory}>{routes}</Router>,
+	</Provider>,
 	document.getElementById('app')
 );
